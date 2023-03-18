5e91b151bffb8ecafb37befc4376f2f8671cb606
Aatif Syed <aatifsyedyp@gmail.com>
2023-03-18 04:19:57 +0000

    test: assert #[from] passes through for thiserror::Error

--- tests/thiserror.rs
+++ tests/thiserror.rs
@@ -1,12 +1,47 @@
-#[err_as_you_go::err_as_you_go(derive(Debug, thiserror::Error))]
-fn foo() -> Result<(), FooError> {
+#![allow(unused)]
+
+use err_as_you_go::err_as_you_go;
+
+#[err_as_you_go(derive(Debug, thiserror::Error))]
+fn simple_string_error() -> Result<(), FooError> {
     Err(err!(
         #[error("no bars :(")]
-        Bar
+        NoBars
+    ))
+}
+
+#[err_as_you_go(derive(Debug, thiserror::Error))]
+fn interpolated_string_error(u: usize) -> Result<(), BarError> {
+    Err(err!(
+        #[error("{0} foos is not enough!")]
+        NotEnoughFoos(usize = u)
+    ))
+}
+
+#[err_as_you_go(derive(Debug, thiserror::Error))]
+fn error_implements_from() -> Result<(), BazError> {
+    Err(err!(
+        #[error("fuck")]
+        IoErr(
+            #[from]
+            std::io::Error = std::io::Error::new(std::io::ErrorKind::PermissionDenied, "blah")
+        )
     ))
 }
 
 #[test]
-fn thiserror_message() {
-    assert_eq!(foo().unwrap_err().to_string(), "no bars :(")
+fn thiserror_message_simple() {
+    assert_eq!(simple_string_error().unwrap_err().to_string(), "no bars :(")
+}
+
+#[test]
+fn thiserror_message_interpolated() {
+    assert_eq!(
+        interpolated_string_error(1).unwrap_err().to_string(),
+        "1 foos is not enough!"
+    )
+}
+
+fn assert_error_implements_from(e: std::io::Error) -> BazError {
+    e.into()
 }
